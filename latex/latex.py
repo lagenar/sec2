b40d404aae30b779fc7e8c1049eaedb769b2a4af
lagenar <lagenar@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-11-03 22:38:18 +0000

    se agrego el sistema de documentacion

---
+++ latex/latex.py
@@ -0,0 +1,38 @@
+doc = r"""\documentclass{article}
+\author{Lucas Moauro}
+\date{}
+\title{Sec II: Sistema de expendio de combustible}
+\begin{document}
+\maketitle
+\newpage
+%(servicios)s\end{document}
+"""
+
+servicio = r"""\section{%(servicio)s}
+%(modulos)s"""
+
+modulo = r"""\subsection{%(modulo)s}
+%(parametros)s\newline
+%(descripcion)s
+"""
+
+parametro = r"""\textbf{%(nombre)s}\newline
+"""
+
+def escape(str):
+    return str.replace('_', r'\_')
+
+def make_doc(services):
+    servs = ''
+    for service in services:
+        modules = ''
+        for module in service['modulos']:
+            pars = ''
+            for p in module['parametros']:
+                pars += escape(parametro % {'nombre':p})
+            modules += modulo % {'modulo':escape(module['nombre']),
+                                 'parametros':pars,
+                                 'descripcion':escape(module['descripcion'][0])}
+        servs += servicio % {'servicio':service['nombre'],
+                                'modulos': modules}
+    return doc % {'servicios':servs}
